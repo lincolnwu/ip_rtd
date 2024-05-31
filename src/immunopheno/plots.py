51a98480d237e7bb2fe4e97757c53a33b5f4148b
lincolnwu <lincolnwu18@gmail.com>
2024-05-31 15:32:57 -0400

    added plot corr function

--- src/immunopheno/plots.py
+++ src/immunopheno/plots.py
@@ -1,14 +1,102 @@
 import seaborn as sns
 import plotly.express as px
 import umap
+import numpy as np
+import pandas as pd
+import scipy.stats as ss
 
 from .data_processing import PlotUMAPError, ImmunoPhenoData
+
+def _correlation_ab(classified_df: pd.DataFrame,
+                    z_scores_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Parameters:
+        classified_df (pd.DataFrame): DataFrame of all cells and their
+            classification as background or signal for a set of antibodies
+            Format: rows: cells, col: antibodies
+        z_scores_df (pd.DataFrame): DataFrame of z-scores of protein counts
+            Format: rows: cells, col: antibodies
+
+    Returns:
+        correlation_df (pd.DataFrame): DataFrame containing pearson's correlation
+            coefficients for each pair of antibodies
+    """
+
+    classified_transpose = classified_df.copy(deep=True).T
+    z_transpose = z_scores_df.copy(deep=True).T
+
+    # Before we start, we want to remove any rows that contain inf or NaN
+    z_transpose.replace([np.inf, -np.inf], np.nan, inplace=True)
+    z_transpose.dropna(axis=0, how='any', inplace=True)
+
+    # Remove same rows from classified
+    classified_transpose = classified_transpose.loc[z_transpose.index]
+
+    ab_names = list(classified_transpose.index)
+    num_ab = len(ab_names)
+
+    correlation_matrix = np.zeros([num_ab, num_ab])
+
+    for i in range(0, num_ab):
+        for j in range(i + 1, num_ab):
+
+            ab_a_zscores = []
+            ab_b_zscores = []
+
+            ab_a = list(classified_transpose.iloc[i])
+            ab_b = list(classified_transpose.iloc[j])
+
+            ab_bool = [x == 0 and y == 0 for x, y in zip(ab_a, ab_b)]
+            ab_pair_bool = list(np.where(ab_bool)[0])
+
+            z_score_a = np.array(z_transpose.iloc[i])
+            z_score_b = np.array(z_transpose.iloc[j])
+
+            # Add all z scores that belong to True in ab_pair_bool
+            ab_a_zscores.extend(z_score_a[ab_pair_bool])
+            # Ignore constant arrays that break the pearson correlation
+            if ab_a_zscores.count(ab_a_zscores[0]) == len(ab_a_zscores):
+                break
+
+            ab_b_zscores.extend(z_score_b[ab_pair_bool])
+            if ab_b_zscores.count(ab_b_zscores[0]) == len(ab_b_zscores):
+                break
+
+            # Calculate pearson correlation from both z score lists
+            correlation = ss.pearsonr(ab_a_zscores, ab_b_zscores)
+
+            # Add value to matrix
+            correlation_matrix[i][j] = correlation[0]
+            correlation_matrix[j][i] = correlation[0]
+            correlation_matrix[i][i] = 1
+            correlation_matrix[i + 1][i + 1] = 1
+
+    correlation_df = pd.DataFrame(correlation_matrix,
+                                  index=ab_names,
+                                  columns=ab_names)
+    return correlation_df
+
+def plot_antibody_correlation(IPD: ImmunoPhenoData):
+    """
+    Plots a correlation heatmap for each antibody in the data
+
+    Parameters:
+        IPD (ImmunoPhenoData Object): Object containing protein data,
+            gene data, and cell types
+    
+    Returns:
+        seaborn clustermap for a heatmap of the antibodies
+    """
+    # Calculate correlation dataframe
+    corr_df = _correlation_ab(IPD._classified_filt_df, IPD._z_scores_df)
+    g = sns.clustermap(corr_df, vmin=-1, vmax=1, cmap='BrBG')
+    g.ax_cbar.set_position((1, .2, .03, .4))
 
 def plot_UMAP(IPD: ImmunoPhenoData,
               normalized: bool = False,
               **kwargs):
     """ Plots a UMAP of protein expression
-    
+
     Plots a UMAP for the non-normalized protein values or normalized protein
     values
 
